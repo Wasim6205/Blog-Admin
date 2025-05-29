a56d6e5fe130fe2dc1e887cd90392df8e3cc952c
Wasim6205 <89179686+Wasim6205@users.noreply.github.com>
2025-05-29 11:02:03 +0530

    Update prodRoute.js

--- client/src/store/prodRoute.js
+++ client/src/store/prodRoute.js
@@ -3,7 +3,7 @@ import {createSlice} from "@reduxjs/toolkit"
 const prodSlice = createSlice({
     name:"prod",
     initialState: {
-        link:"http://localhost:1000"
+        link:"https://blog-admin-backend-kbem.onrender.com"
     }
 })
 
